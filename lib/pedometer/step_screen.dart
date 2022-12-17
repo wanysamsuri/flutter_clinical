fca6904242c9e999e225cfe0c6e5308c1fa6ca7b
Wany <wanysamsuri1@gmail.com>
2022-12-17 12:36:28 +0800

    pedometer ui

--- lib/pedometer/step_screen.dart
+++ lib/pedometer/step_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -106,11 +107,18 @@ class _StepMainScreenState extends State<StepMainScreen> {
                     width: screenWidth,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(12),
-                        color: Colors.grey[300]),
+                        color: secondaryColor,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey, //New
+                              blurRadius: 10.0,
+                              offset: Offset(0, 0))
+                        ]),
                     child: Container(
+                      padding: EdgeInsets.all(12),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Column(
                             // crossAxisAlignment: CrossAxisAlignment.start,
@@ -122,17 +130,99 @@ class _StepMainScreenState extends State<StepMainScreen> {
                                 ),
                               ),
                               Container(
-                                child: Text('1000'),
+                                child: Text(
+                                  '1001',
+                                  style: TextStyle(fontSize: 30),
+                                ),
                               )
                             ],
                           ),
                           Container(
-                            child: Text('1000'),
-                          )
+                              // child: LineChart(LineChartData(
+                              //     minX: 0,
+                              //     maxX: 5,
+                              //     minY: 0,
+                              //     maxY: 5,
+                              //     lineBarsData: [
+                              //       LineChartBarData(spots: [FlSpot(0, 3)])
+                              //     ])),
+                              )
                         ],
                       ),
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Previous',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Icon(Icons.sort),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(Adaptive.w(2)),
+                    // height: Adaptive.h(30),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          ListView.builder(
+                              shrinkWrap: true,
+                              physics: const BouncingScrollPhysics(),
+                              scrollDirection: Axis.vertical,
+                              itemCount: 5,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 1.0, vertical: 10),
+
+                                  //body listview
+                                  child: Container(
+                                    height: Adaptive.h(18),
+                                    width: screenWidth,
+                                    // height: Adaptive.h(40),
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(20),
+                                      // boxShadow: [
+                                      //   BoxShadow(
+                                      //       color: Colors.grey, //New
+                                      //       blurRadius: 10.0,
+                                      //       offset: Offset(-1, -1))
+                                      // ],
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        // SizedBox(
+                                        //   height: 1,
+                                        // ),
+                                        Container(
+                                          padding: EdgeInsets.all(15),
+                                          child: Text(
+                                            'Test',
+                                            textAlign: TextAlign.justify,
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              // fontWeight:
+                                              //     FontWeight.bold,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }),
+                        ]),
+                  ),
                 ],
               ),
             ),
