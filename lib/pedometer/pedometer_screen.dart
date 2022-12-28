9b58606adff874e1e4a6fec48787c0b3d2cdce26
Wany <wanysamsuri1@gmail.com>
2022-12-28 12:53:27 +0800

    pedometer counter

--- lib/pedometer/pedometer_screen.dart
+++ lib/pedometer/pedometer_screen.dart
@@ -1,74 +1,155 @@
-import 'dart:ui';
+import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_clinic/constant.dart';
-import 'package:flutter_clinic/screens/profile/terms_condition.dart';
-import 'package:flutter_rating_stars/flutter_rating_stars.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:hive/hive.dart';
+import 'package:jiffy/jiffy.dart';
 import 'package:pedometer/pedometer.dart';
-
-import '../../services/api_service.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:intl/intl.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PedometerScreen extends StatefulWidget {
-  const PedometerScreen({Key? key}) : super(key: key);
+  const PedometerScreen({super.key});
 
   @override
   State<PedometerScreen> createState() => _PedometerScreenState();
 }
 
 class _PedometerScreenState extends State<PedometerScreen> {
-  TextEditingController _messageController = TextEditingController();
+  // final catsBox = collection.openBox<Map>('cats');
   late Stream<StepCount> _stepCountStream;
   late Stream<PedestrianStatus> _pedestrianStatusStream;
-  String _status = '', _steps = '';
+  late StreamSubscription _subscription;
+  late String _status = '';
+  late int _todaySteps = 0;
+  late int _steps = 0;
+  late DateTime _timeStamp = DateTime.now();
+  // late String a = '';
 
   @override
   void initState() {
-    super.initState();
+    // _stepCountStream;
+    // _pedestrianStatusStream;
+    // _status;
+    // _steps;
+
     initPlatformState();
+    super.initState();
   }
 
-  void onStepCount(StepCount event) {
-    print(event);
+  // @override
+  // void dispose() {
+  //   stopCount();
+  //   super.dispose();
+  // }
+
+  // void onTotalStepCount(StepCount event) {
+  //   if (kDebugMode) {
+  //     print(event);
+  //   }
+  //   setState(() {
+  //     // a = event.timeStamp.toString();
+  //     _totalSteps = event.totalSteps;
+  //   });
+  // }
+
+  Future<int> onStepCount(StepCount event) async {
+    var stepsBox = await Hive.openBox('steps');
+
+    _steps = event.steps;
+    if (kDebugMode) {
+      print(event);
+    }
+    int savedStepsCountKey = 999999;
+    int? savedStepsCount = stepsBox.get(savedStepsCountKey, defaultValue: 0);
+
+    if (_steps < savedStepsCount!) {
+      // Upon device reboot, pedometer resets. When this happens, the saved counter must be reset as well.
+      savedStepsCount = 0;
+      // persist this value using a package of your choice here
+      stepsBox.put(savedStepsCountKey, savedStepsCount);
+    }
+    // load the last day saved using a package of your choice here
+    int todayDayNo = Jiffy(DateTime.now()).dayOfYear;
+    int lastDaySavedKey = 888888;
+    int? lastDaySaved = stepsBox.get(lastDaySavedKey, defaultValue: 0);
+
+    // When the day changes, reset the daily steps count
+    // and Update the last day saved as the day changes.
+    if (lastDaySaved! < todayDayNo) {
+      lastDaySaved = todayDayNo;
+      savedStepsCount = _steps;
+
+      stepsBox
+        ..put(lastDaySavedKey, lastDaySaved)
+        ..put(savedStepsCountKey, savedStepsCount);
+    }
     setState(() {
-      _steps = event.steps.toString();
+      _todaySteps = _steps - savedStepsCount!;
     });
+    stepsBox.put(todayDayNo, _todaySteps);
+
+    return _todaySteps;
   }
 
   void onPedestrianStatusChanged(PedestrianStatus event) {
-    print(event);
+    if (kDebugMode) {
+      print(event);
+    }
     setState(() {
       _status = event.status;
     });
   }
 
   void onPedestrianStatusError(error) {
-    print('onPedestrianStatusError: $error');
+    if (kDebugMode) {
+      print('onPedestrianStatusError: $error');
+    }
     setState(() {
       _status = 'Pedestrian Status not available';
     });
-    print(_status);
+    if (kDebugMode) {
+      print(_status);
+    }
   }
 
   void onStepCountError(error) {
-    print('onStepCountError: $error');
-    setState(() {
-      _steps = 'Step Count not available';
-    });
+    if (kDebugMode) {
+      print('onStepCountError: $error');
+    }
+    setState(() {});
   }
 
-  void initPlatformState() {
-    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
-    _pedestrianStatusStream
-        .listen(onPedestrianStatusChanged)
-        .onError(onPedestrianStatusError);
+  void _onDone() => print("Finished pedometer tracking");
 
-    _stepCountStream = Pedometer.stepCountStream;
-    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+  Future<void> initPlatformState() async {
+    if (await Permission.activityRecognition.request().isGranted) {
+      _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+      _stepCountStream = Pedometer.stepCountStream;
+      _subscription = _stepCountStream.listen(onStepCount,
+          onDone: _onDone, onError: onStepCountError, cancelOnError: true);
 
-    if (!mounted) return;
+      // _stepCountStream.listen(onStepCount).onError(onStepCountError);
+      _pedestrianStatusStream
+          .listen(onPedestrianStatusChanged)
+          .onError(onPedestrianStatusError);
+
+      if (kDebugMode) {
+        print(_steps);
+        print(_status);
+      }
+    } else {
+      if (kDebugMode) {
+        print('Tidak dibenarkan');
+      }
+    }
+    // if (!mounted) return;
+  }
+
+  void stopCount() {
+    _subscription.cancel();
   }
 
   @override
@@ -76,7 +157,6 @@ class _PedometerScreenState extends State<PedometerScreen> {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
             automaticallyImplyLeading: false,
             toolbarHeight: screenHeight * 0.07,
@@ -105,81 +185,89 @@ class _PedometerScreenState extends State<PedometerScreen> {
             )),
         body: SafeArea(
             child: SingleChildScrollView(
-          // physics: BouncingScrollPhysics(),
           child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(Adaptive.w(5)),
-                  height: Adaptive.h(15),
-                  width: Adaptive.w(100),
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 157, 228, 234),
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(40),
-                          bottomRight: Radius.circular(40))),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: Center(
-                              child: Image.asset('assets/footstep.png',
-                                  height: 60)),
-                        ),
-                        SizedBox(
-                          height: Adaptive.h(1),
-                        ),
-                      ]),
-                ),
-                SizedBox(
-                  height: Adaptive.h(10),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'Steps taken:',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        _steps,
-                        style: TextStyle(fontSize: 60),
-                      ),
-                      Divider(
-                        height: 100,
-                        thickness: 0,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        'Pedestrian status:',
-                        style: TextStyle(fontSize: 20),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                padding: EdgeInsets.all(Adaptive.w(5)),
+                // height: Adaptive.h(15),
+                width: Adaptive.w(100),
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 157, 228, 234),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40))),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Center(
+                            child:
+                                Image.asset('assets/footstep.png', height: 60)),
                       ),
                       SizedBox(
-                        height: Adaptive.h(2),
+                        height: Adaptive.h(1),
                       ),
-                      Icon(
-                        _status == 'walking'
-                            ? Icons.directions_walk
-                            : _status == 'stopped'
-                                ? Icons.accessibility_new
-                                : Icons.error,
-                        size: 50,
+                    ]),
+              ),
+              SizedBox(
+                height: Adaptive.h(6),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Steps taken at:',
+                      style: TextStyle(fontSize: 26),
+                    ),
+                    Text(
+                      _timeStamp.toString(),
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    const Divider(
+                      height: 100,
+                      thickness: 0,
+                      color: Colors.white,
+                    ),
+                    const Text(
+                      'Steps taken:',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      _todaySteps.toString(),
+                      style: const TextStyle(fontSize: 60),
+                    ),
+                    const Divider(
+                      height: 100,
+                      thickness: 0,
+                      color: Colors.white,
+                    ),
+                    const Text(
+                      'Pedestrian status:',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Icon(
+                      _status == 'walking'
+                          ? Icons.directions_walk
+                          : _status == 'stopped'
+                              ? Icons.accessibility_new
+                              : Icons.error,
+                      size: 100,
+                    ),
+                    Center(
+                      child: Text(
+                        _status,
+                        style: _status == 'walking' || _status == 'stopped'
+                            ? const TextStyle(fontSize: 30)
+                            : const TextStyle(fontSize: 20, color: Colors.red),
                       ),
-                      Center(
-                        child: Text(
-                          _status,
-                          style: _status == 'walking' || _status == 'stopped'
-                              ? TextStyle(fontSize: 30)
-                              : TextStyle(fontSize: 20, color: Colors.red),
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+                    // ElevatedButton(onPressed: () {}, child: Text('Start'))
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ]),
           ),
         )));
   }
